934541b8409b7f4e599e6c3829093afa922db0a1
Koh Wen Jun <kohwenjun13@gmail.com>
2026-02-23 16:50:23 +0800

    Initial ASP.NET Core MVC setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationLab.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
